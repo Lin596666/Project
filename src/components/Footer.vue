bd77c055cb40a79209add9b5c5cffb7d8e61f95b
shaka <10120571+shaka123@user.noreply.gitee.com>
2023-05-15 21:53:03 +0800

    feat code commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!--<table width="1226px" align="center">
+    <tr>
+      <td style="vertical-align: top">
+       <h3>欢迎来到商城系统</h3>
+      </td>
+    </tr>
+
+  </table>-->
+</template>
+
+<style scoped>
+div img {
+  width: 56px;
+}
+
+.n-text-top {
+  color: #757575;
+  font-size: 12px
+}
+</style>
